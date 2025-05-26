18fcad9dcc0af8ed9e4861651bceaebc0d613581
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-05-26 23:34:10 +0500

    Update firebase_auth_services.dart

--- lib/data/firebase_auth_services.dart
+++ lib/data/firebase_auth_services.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
-
 class FirebaseAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
