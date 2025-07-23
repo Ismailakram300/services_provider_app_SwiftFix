8acd71852c27db8bda594548ff0b9872074511b7
Ismailakram300 <ismailakram300@gmail.com>
2025-07-23 11:51:04 +0500

    Update

--- lib/MobileScreens/splash_screen.dart
+++ lib/MobileScreens/splash_screen.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home Momina"S" Project ',
+      title: 'Home Mominas" Project ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
