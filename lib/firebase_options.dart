0addfba47b8323565140bca89ac28bc3368ec392
Ismail Akram <123332697+Ismailakram300@users.noreply.github.com>
2025-09-09 23:20:35 +0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -12,7 +12,7 @@ import 'package:flutter/foundation.dart'
 /// ```dart
 /// import 'firebase_options.dart';
 /// // ...
-//"mo
+//"m
 /// await Firebase.initializeApp(
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
