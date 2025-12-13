4eec3a9c9d25a681045a9ff718a913addb3517aa
Ismail Akram <123332697+Ismailakram300@users.noreply.github.com>
2025-12-13 15:03:43 +0500

    Remove unnecessary blank lines in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:serviceapp/MobileScreens/extra.dart';
 import 'package:serviceapp/MobileScreens/login.dart';
 import 'package:serviceapp/MobileScreens/offers.dart';
